714f72b7d91752382a5628a7cbf639b8196cbef6
JigerSingh <124743224+JigerSinghh@users.noreply.github.com>
2024-01-08 17:55:41 -0500

    Update Program.cs

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Pick a number!");
+            var answer = 4;
+            var response = int.Parse (Console.ReadLine());
+          
+
+            var userResponse = (response >= 4) ? ($"{response} is greater than or equal to {answer}") : ($"{response} is less than {answer}");
+
+            Console.WriteLine(userResponse);
+
+
         }
     }
 }
